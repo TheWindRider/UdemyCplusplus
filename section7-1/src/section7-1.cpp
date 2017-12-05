d69f193a95f8b6a4cc59b86ec4e8e5fce9d2e1f8
Chenguang Yang <ychg235711@gmail.com>
2017-12-04 20:06:51 -0800

    Add lesson 7 about function pointer

---
+++ section7-1/src/section7-1.cpp
@@ -0,0 +1,47 @@
+//============================================================================
+// Name        : section7-1.cpp
+// Author      : Chenguang Yang
+
+// Description : Udemy exercise
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool matchSize(string input) {
+    return input.size() == 3;
+}
+
+bool matchTrend(string before, string after) {
+    return after.size() >= before.size();
+}
+
+/* note the syntax to use function as parameter */
+int my_count_if(vector<string> &strVec, bool (*match)(string first, string second)) {
+    int total = 0;
+    for (int i = 1; i < strVec.size(); i++) {
+        if (matchTrend(strVec[i-1], strVec[i])) {
+            total += 1;
+        }
+    }
+    return total;
+}
+
+int main()
+{
+    vector<string> strVec;
+    strVec.push_back("one");
+    strVec.push_back("two");
+    strVec.push_back("three");
+    strVec.push_back("four");
+    strVec.push_back("five");
+
+    /* custom criteria */
+    cout << count_if(strVec.begin(), strVec.end(), matchSize) << endl; // function name is the pointer
+    /* custom criteria and function */
+    cout << my_count_if(strVec, matchTrend) << endl;
+
+    return 0;
+}
